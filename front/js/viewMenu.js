1af23e98e3b876726b620343300d514b282e1793
Álvaro Gutiérrez García <alvarogutierrezg99@gmail.com>
2023-04-01 01:53:24 +0200

    a

--- front/js/viewMenu.js
+++ front/js/viewMenu.js
@@ -1,5 +1,13 @@
 
 function loadView(component,data){
+
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = "css/elementPreview.css";
+    const head = document.getElementsByTagName('head')[0];
+    head.appendChild(link);
+    
     for (let i = 0; i < data.length; i++) {
         document.body.innerHTML = document.body.innerHTML.replace("<!--replaceElementPreview-->" , component + "\n" + "<!--replaceElementPreview-->")
     }
